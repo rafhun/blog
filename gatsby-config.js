439bb28c4a747a0f9dd18262e1bcc225ab59edca
Raphael Hüni <hueni@werbelinie.ch>
2020-10-27 17:11:01 +0100

    Add r-logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#090088`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/r-logo.svg`,
       },
     },
     `gatsby-plugin-react-helmet`,
